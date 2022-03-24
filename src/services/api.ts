a82e79e8b9c8141632934a759c03fdd4c14b2ab0
Vitor Varjão <vitornvj@gmail.com>
2022-03-23 22:54:12 -0300

    config: config .env for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { parseCookies } from 'nookies'
 
 const cookies = parseCookies()
 export const api = axios.create({
-    baseURL: process.env.API_URL ||'http://localhost:5050/api/',
+    baseURL: process.env.API_URL,
     headers:{
         'x-access-token': `${cookies['cabelex.accessToken']}`
     }
